c5a76c093031fff4c630c4a8498dae6301500576
daryll <dosis@csumb.edu>
2018-04-25 22:45:05 -0700

    added some styling in show_court.html

--- apps/hoopfinder/models.py
+++ apps/hoopfinder/models.py
@@ -59,3 +59,12 @@ class User(models.Model):
     objects = UserManager()
 
 
+class Courts(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=75)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=5)
+    imagelink = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
